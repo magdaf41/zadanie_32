45884004e19e82a42ad8eaf3cf879c747002e6c1
Magdalena Falkowska-Legieć <magda.f41@gmail.com>
2020-08-10 19:10:30 +0200

    zadanie_32 - poprawione

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ var num2 = 5;
 
 function myFunc(num1, num2) {
     let result = num1 == num2;
+    if (num1 > num2) {
+        console.log("2 jest większe od 5");
+    }
+    else {
+        console.log('5 jest większe od 2');
+    }
     return result;
 }
-if (num1 > num2) {
-    console.log("2 jest większe od 5");
-}
-else {
-    console.log('5 jest większe od 2');
-}
 
+console.log(myFunc(num1, num2));
